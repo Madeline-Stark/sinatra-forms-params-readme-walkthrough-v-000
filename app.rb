dad72f47b0d133c5fac424c9ee7f7c3df644bdf3
Madeline Stark <madeline.stark1@gmail.com>
2018-09-10 19:09:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,6 +10,9 @@ class App < Sinatra::Base
     params.to_s
   end
 
-  # Add your post route and action below
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+
 
 end
